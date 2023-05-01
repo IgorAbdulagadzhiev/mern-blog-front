39b26df1bb04cb2051a807ebd0f9e830e7d8f03a
Igor Abdulagadzhiev <s89183906500@gmail.com>
2023-05-01 18:39:34 +0300

    add tags

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -7,6 +7,11 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
   return data;
 });
 
+export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
+  const { data } = await axios.get('/tags');
+  return data;
+});
+
 const initialState = {
   posts: {
     items: [],
@@ -33,6 +38,18 @@ const postsSlice = createSlice({
     [fetchPosts.rejected]: (state) => {
       state.posts.items = [];
       state.posts.status = ERROR;
+    },
+    [fetchTags.pending]: (state) => {
+      state.tags.items = [];
+      state.tags.status = LOADING;
+    },
+    [fetchTags.fulfilled]: (state, action) => {
+      state.tags.items = action.payload;
+      state.tags.status = LOADED;
+    },
+    [fetchTags.rejected]: (state) => {
+      state.tags.items = [];
+      state.tags.status = ERROR;
     },
   },
 });
